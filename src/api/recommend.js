474a43f7b051c3380dba7ae0b37b18ec560e3478
Cphayim <admin@cphayim.me>
2018-01-02 15:56:48 +0800

    feat: 歌单列表请求接口和转发接口

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -2,11 +2,12 @@
  * @Author: Cphayim
  * @Date: 2017-12-12 18:11:12
  * @Last Modified by: Cphayim
- * @Last Modified time: 2017-12-12 19:32:33
+ * @Last Modified time: 2018-01-02 13:16:34
  */
 
-import jsonp from 'common/js/jsonp'
+import jsonp from 'cy-jsonp'
 import { commonParams, options } from './config'
+import axios from 'axios'
 
 /**
  * @function getRecommend
@@ -23,4 +24,31 @@ export function getRecommend() {
   })
 
   return jsonp(url, data, options)
+}
+
+/**
+ * @function getDiscList
+ * @desc 获取歌单列表
+ * @return {Promise}
+ */
+export function getDiscList() {
+  const url = '/api/getDiscList'
+
+  const data = Object.assign({}, commonParams, {
+    platform: 'yqq',
+    hostUin: 0,
+    sin: 0,
+    ein: 29,
+    sortId: 5,
+    needNewCode: 0,
+    categoryId: 10000000,
+    rnd: Math.random(),
+    format: 'json'
+  })
+
+  return axios.get(url, {
+    params: data
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
 }
